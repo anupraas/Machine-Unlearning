b7851d2e6629e91282fec99e565c6b17a4f5b236
Anupraas Gautam <anupraas95@gmail.com>
2020-03-29 17:15:46 -0500

    plots

--- auto_sklearn_example.py
+++ auto_sklearn_example.py
@@ -1,24 +1,21 @@
 import autosklearn.classification
-
 import sklearn.model_selection
-
 import sklearn.datasets
-
 import sklearn.metrics
-
 import numpy as np
 
-X, y = sklearn.datasets.load_digits(n_class=2, return_X_y=True)
+#X, y = sklearn.datasets.load_digits(n_class=2, return_X_y=True)
+X, y = sklearn.datasets.load_digits(return_X_y=True)
 print("Dataset loaded")
 print("X: {}".format(np.shape(X)))
 print("y: {}".format(np.shape(y)))
 X_train, X_test, y_train, y_test = \
         sklearn.model_selection.train_test_split(X, y, random_state=1)
 
-automl = autosklearn.classification.AutoSklearnClassifier()
-
+automl = autosklearn.classification.AutoSklearnClassifier(time_left_for_this_task=30)
 automl.fit(X_train, y_train)
 print("Model trained")
 y_hat = automl.predict(X_test)
 print("Predictions done")
 print("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
+print(automl.show_models())
