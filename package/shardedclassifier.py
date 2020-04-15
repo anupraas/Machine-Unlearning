61006198f7237c534b83b3e1d4286a46f7f9b2e3
anupraas <agautam6@wisc.edu>
2020-04-15 17:58:38 -0500

    All public for now..

--- package/shardedclassifier.py
+++ package/shardedclassifier.py
@@ -21,8 +21,8 @@ class VanillaShardedClassifier:
         self.ml_algorithm = ml_algorithm
 
         # Training data
-        self.__X_train = None
-        self.__y_train = None
+        self.X_train = None
+        self.y_train = None
 
         # Book-keeping
         #   shard_data_dict: dictionary {shard_id, <list>}
@@ -33,27 +33,27 @@ class VanillaShardedClassifier:
         #       - reverse mapping of each point in training data to shard in which it lies
         #       - this is currently a hack to find shard during unlearning in O(1)
         #       - ideally this should be replaced by binary search
-        self.__shard_data_dict = {}
-        self.__shard_model_dict = {}
-        self.__data_to_shard_dict = {}
+        self.shard_data_dict = {}
+        self.shard_model_dict = {}
+        self.data_to_shard_dict = {}
 
         # Default prediction in case of 0 points in training set
-        self.__default_class = None
+        self.default_class = None
 
     def fit(self, X, y):
-        self.__X_train = copy.deepcopy(X)
-        self.__y_train = copy.deepcopy(y)
-        self.__default_class = Counter(y).most_common(1)[0][0]
-        self.__initialize_bookkeeping_dicts()
-        for shard_num in self.__shard_data_dict:
-            self.__shard_model_dict[shard_num] = self.__fit_shard(self.__X_train[self.__shard_data_dict[shard_num]],
-                                                                  self.__y_train[self.__shard_data_dict[shard_num]])
+        self.X_train = copy.deepcopy(X)
+        self.y_train = copy.deepcopy(y)
+        self.default_class = Counter(y).most_common(1)[0][0]
+        self.initialize_bookkeeping_dicts()
+        for shard_num in self.shard_data_dict:
+            self.shard_model_dict[shard_num] = self.fit_shard(self.X_train[self.shard_data_dict[shard_num]],
+                                                              self.y_train[self.shard_data_dict[shard_num]])
 
     # Prediction - vanilla implementation: taking simple majority vote
     def predict(self, X):
         predictions = []
-        for m in self.__shard_model_dict:
-            predictions.append(self.__shard_model_dict[m].predict(X))
+        for m in self.shard_model_dict:
+            predictions.append(self.shard_model_dict[m].predict(X))
         predictions = np.asarray(predictions)
         ret_predictions = [Counter(predictions[:, i]).most_common(1)[0][0] for i in range(len(X))]
         return ret_predictions
@@ -61,36 +61,36 @@ class VanillaShardedClassifier:
     # Unlearning - vanilla implementation: remove point and refit model
     #               (in case of auto sklearn refit causes rediscovery with bayesian optimizer)
     def unlearn(self, X_y_ids):
-        shard_num = self.__getShardNum(X_y_ids)
+        shard_num = self.getShardNum(X_y_ids)
         for i in range(len(X_y_ids)):
-            self.__shard_data_dict[shard_num[i]].remove(X_y_ids[i])
+            self.shard_data_dict[shard_num[i]].remove(X_y_ids[i])
         for shard_i in list(set(shard_num)):
-            self.__shard_model_dict[shard_i] = self.__fit_shard(self.__X_train[self.__shard_data_dict[shard_i]],
-                                                                self.__y_train[self.__shard_data_dict[shard_i]])
+            self.shard_model_dict[shard_i] = self.fit_shard(self.X_train[self.shard_data_dict[shard_i]],
+                                                            self.y_train[self.shard_data_dict[shard_i]])
 
-    def __fit_shard(self, X, y):
+    def fit_shard(self, X, y):
         if len(X) is 0:
-            return self.__DummyClassifier(prediction=self.__default_class)
+            return self.DummyClassifier(prediction=self.default_class)
         elif len(Counter(y).keys()) is 1:
-            return self.__DummyClassifier(prediction=y[0])
+            return self.DummyClassifier(prediction=y[0])
         else:
             return self.ml_algorithm.fit(X, y)
 
     #   Creates class-balanced separations of training data and assigns to each shard
-    def __initialize_bookkeeping_dicts(self):
-        y = self.__y_train
+    def initialize_bookkeeping_dicts(self):
+        y = self.y_train
         manager = [0] * len(Counter(y).keys())
-        self.__shard_data_dict = {sh_num: [] for sh_num in range(self.num_shards)}
+        self.shard_data_dict = {sh_num: [] for sh_num in range(self.num_shards)}
         for it in range(len(y)):
-            self.__shard_data_dict[manager[y[it]]].append(it)
-            self.__data_to_shard_dict[it] = manager[y[it]]
+            self.shard_data_dict[manager[y[it]]].append(it)
+            self.data_to_shard_dict[it] = manager[y[it]]
             manager[y[it]] = (manager[y[it]] + 1) % self.num_shards
-        self.__shard_model_dict = {sh_num: None for sh_num in range(self.num_shards)}
+        self.shard_model_dict = {sh_num: None for sh_num in range(self.num_shards)}
 
-    def __getShardNum(self, idx):
-        return [self.__data_to_shard_dict[id_i] for id_i in idx]
+    def getShardNum(self, idx):
+        return [self.data_to_shard_dict[id_i] for id_i in idx]
 
-    class __DummyClassifier:
+    class DummyClassifier:
         prediction = 0
 
         def __init__(self, prediction):
@@ -98,5 +98,3 @@ class VanillaShardedClassifier:
 
         def predict(self, X):
             return [self.prediction] * len(X)
-
-
