31e2958d853e09f2a324458baf0cb45fd71f5394
Anupraas Gautam <anupraas95@gmail.com>
2020-04-19 18:51:37 -0500

    update

--- package/autoshardedclassifier.py
+++ package/autoshardedclassifier.py
@@ -43,7 +43,7 @@ class AutoShardedClassifier:
         print(self.num_shards)
         self.ml_algorithm = autosklearn.classification.AutoSklearnClassifier(time_left_for_this_task=300,
                                                                              ensemble_size=self.num_shards,
-                                                                             ensemble_nbest=self.num_shards,
+                                                                             ensemble_nbest=max(2*self.num_shards, 50),
                                                                              ensemble_memory_limit=4096,
                                                                              include_preprocessors=['no_preprocessing'])
         best_models_ensemble = self.ml_algorithm.fit(self.X_train, self.y_train).get_models_with_weights()
