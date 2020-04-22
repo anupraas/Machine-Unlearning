05503f383b48533a6b383d1880e7f0a937fde69f
Anupraas Gautam <anupraas95@gmail.com>
2020-04-21 19:41:38 -0500

    typo

--- testAll.py
+++ testAll.py
@@ -37,7 +37,7 @@ def preprocess_data(X, y, samplesize=None):
 
 all_number_of_shards = [1, 5, 10, 20, 50, 100, 200]
 MLAs = [
-#    autoshardedclassifier.AutoShardedClassifier(),
+    autoshardedclassifier.AutoShardedClassifier(),
     # AdaBoostClassifier(),
     # BernoulliNB(),
     DecisionTreeClassifier(),
