34cafaeb955ae3494f35473ef25775f75d14d21b
anupraas <agautam6@wisc.edu>
2020-04-23 00:14:14 -0500

    test part 2

--- testMnistAuto.py
+++ testMnistAuto.py
@@ -35,8 +35,8 @@ def preprocess_data(X, y, samplesize=None):
     return X, y
 
 
-all_number_of_shards = [1, 5, 10, 20, 50, 100, 200]
-# all_number_of_shards = [100, 200]
+# all_number_of_shards = [1, 5, 10, 20, 50, 100, 200]
+all_number_of_shards = [50, 100, 200]
 MLAs = [
     autoshardedclassifier.AutoShardedClassifier(),
     # AdaBoostClassifier(),
