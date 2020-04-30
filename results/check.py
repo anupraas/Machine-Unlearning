7e9de433acc4dfd4a0e0849adfbccbb50d49df87
anupraas <agautam6@wisc.edu>
2020-04-29 21:20:26 -0500

    Update check.py

--- results/check.py
+++ results/check.py
@@ -1,11 +1,11 @@
 import pickle
 import numpy as np
 
-with open('covtype_compare_ens5_part_1', 'rb') as f:
-	a=pickle.load(f)
+with open('raw/covtype', 'rb') as f:
+    a = pickle.load(f)
 # print(a)
 for num_shard in a.keys():
-	print(num_shard)
-	for mla in a[num_shard].keys():
-		print('\t{}:'.format(mla))
-		print(a[num_shard][mla])
+    print(num_shard)
+    for mla in a[num_shard].keys():
+        print('\t{}:'.format(mla))
+        print(a[num_shard][mla])
